f0b8df7308b536ab3c695d51b2a665a5a3fc6aec
Jeremy Philemon <jeremyphilemon@outlook.com>
2017-04-18 19:27:43 +0530

    Changed the length of the name.
    The name's length was changed from 50 to 1000.

--- genre/models.py
+++ genre/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
 class Genre(MPTTModel):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=1000, unique=True)
     def __str__(self):
         return 'Genre: ' + self.name
     parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
